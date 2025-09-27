486d5c7102145d28bf43240988e10d5f130a6224
djibr <d.korchi@lprs.fr>
2025-09-27 16:26:39 +0200

    menu d'entrée

--- src/main.c
+++ src/main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void afficher_intro() {
+    printf("=====================================\n");
+    printf("        Bienvenue dans OceanDepth \n");
+    printf("=====================================\n");
+    printf("\n");
+    printf("Souhaitez-vous commencer une nouvelle partie ? (oui/non)\n");
+    printf("> ");
+}
 int main()
 {
-    printf("Hello, World!\n");
+    char *choix = malloc(sizeof(char)*100);
+    int fin = 1;
+    afficher_intro();
+    scanf("%s", choix);
+    while (fin) {
+        if (strcmp(choix, "oui") == 0 || strcmp(choix, "OUI") == 0) {
+            printf("\nParfait ! Préparez votre harpon... l’aventure commence !\n");
+        } else if (strcmp(choix, "non") == 0 || strcmp(choix, "NON") == 0) {
+            printf("\nVous avez choisi de rester en surface. À bientôt ! \n");
+            fin = 0;
+        } else {
+            printf("\nRéponse invalide. Veuillez taper 'oui' ou 'non'.\n");
+        }
+    }
+
+
     return 0;
 }
